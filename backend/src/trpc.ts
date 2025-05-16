f22f39a1e41f612f06c23e58ab3faf9519dc4f5f
belokonandrey <andrey.belokon@synchro.pro>
2025-05-16 20:44:08 +0300

    BE: Trpc endpoint

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,11 +1,13 @@
 import { initTRPC } from '@trpc/server';
 import _ from 'lodash';
+import z from 'zod';
 
 const ideas = _.times(100, (i) => ({
   id: i,
   nick: `some-nick-${i}`,
   name: `element ${i}`,
   description: `Element ${i}`,
+  text: _.times(11, () => '<span>Lorem ipsum dolor.</span>').join(''),
 }));
 
 const trpc = initTRPC.create();
@@ -21,6 +23,17 @@ export const trpcRouter = trpc.router({
       ),
     };
   }),
+
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+      return { idea: idea || null };
+    }),
 });
 
 export type TrpcRouter = typeof trpcRouter;
--- webapp/src/pages/ViewIdea/index.tsx
+++ webapp/src/pages/ViewIdea/index.tsx
@@ -1,39 +1,31 @@
 import { useParams } from 'react-router-dom';
 import { ViewIdeaRouteParams } from '../../lib/routes.ts';
+import { trpc } from '../../lib/trpc.tsx';
 
 const ViewIdeaPage = () => {
   const { ideaNick } = useParams() as ViewIdeaRouteParams;
 
+  const { data, isLoading, error, isError, isFetching } = trpc.getIdea.useQuery(
+    { ideaNick }
+  );
+
+  if (isLoading || isFetching) {
+    return <div>loading....</div>;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!data?.idea) {
+    return <span>idea not found</span>;
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>description</p>
-      <div>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-          autem doloremque, eaque eius excepturi explicabo ipsum libero nisi
-          odit recusandae. Commodi, explicabo perferendis! Aspernatur facilis
-          maxime quisquam quo rem temporibus?
-        </div>
-        <div>
-          Alias consequatur, dolore eaque et quas rerum tenetur voluptatibus.
-          Accusamus beatae consequatur corporis delectus dignissimos explicabo
-          facere incidunt pariatur quaerat recusandae. Aut ducimus eos et, fuga
-          in pariatur ratione rem!
-        </div>
-        <div>
-          Ab deleniti, dolore earum enim et exercitationem quasi velit!
-          Assumenda, dolorem et nam nihil ratione saepe sequi. Ab accusantium
-          aliquam animi aspernatur blanditiis commodi dolores, eos, iste, modi
-          neque possimus.
-        </div>
-        <div>
-          Alias asperiores aspernatur distinctio incidunt ipsa minus mollitia
-          nam, necessitatibus, neque nisi obcaecati officia omnis perspiciatis
-          praesentium quia quibusdam quod reprehenderit rerum sit tenetur unde
-          velit vitae voluptas? Ratione, unde.
-        </div>
-      </div>
+      <h1>{data?.idea.nick}</h1>
+      <p>{data?.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data?.idea.text }}></div>
     </div>
   );
 };
