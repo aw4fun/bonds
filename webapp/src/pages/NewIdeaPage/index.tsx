f8c8699dc406bc9af08ec9e3c1973844ec1c6f20
belokonandrey <andrey.belokon@synchro.pro>
2025-05-22 20:26:45 +0300

    FE: Added form inputs

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+import Segment from '../../components/Segment';
+import Input from '../../components/Input';
+import TextArea from '../../components/TextArea';
+
 const NewIdeaPage = () => {
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  });
+
   return (
-    <div>
-      <h1>new idea</h1>
-      <div>form will be here</div>
-    </div>
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // eslint-disable-next-line no-console
+          console.info('Submitted', state);
+        }}
+      >
+        <Input name="name" label="Name" state={state} setState={setState} />
+
+        <Input name="nick" label="Nick" state={state} setState={setState} />
+
+        <Input
+          name="description"
+          label="Description"
+          state={state}
+          setState={setState}
+        />
+
+        <TextArea name="text" label="Text" state={state} setState={setState} />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
   );
 };
 
