6673b418363d7ed7371296c01292634c0fc7dd39
belokonandrey <andrey.belokon@synchro.pro>
2025-05-28 19:21:20 +0300

    BE: Added zod schema constant

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -4,29 +4,15 @@ import TextArea from '../../components/TextArea';
 import { useFormik } from 'formik';
 
 import { withZodSchema } from 'formik-validator-zod';
-import z from 'zod';
 import { trpc } from '../../lib/trpc.tsx';
+import { zCreateIdeaTrpcInput } from '@bonds/backend/src/router/createIdea/input.ts';
 
 const NewIdeaPage = () => {
   const createIdea = trpc.createIdea.useMutation();
 
   const formik = useFormik({
     initialValues: { name: '', nick: '', description: '', text: '' },
-
-    validate: withZodSchema(
-      z.object({
-        name: z.string().min(1),
-        nick: z
-          .string()
-          .min(1)
-          .regex(
-            /^[a-z0-9-]+$/,
-            'Nick may contain only lowercase letters, numbers and dashes'
-          ),
-        description: z.string().min(1),
-        text: z.string().min(15, 'Text should be at least 100 characters long'),
-      })
-    ),
+    validate: withZodSchema(zCreateIdeaTrpcInput),
     onSubmit: async (values) => {
       await createIdea.mutateAsync(values);
     },
